5e967d1c1bc8d85c1386fa545bc7c937314b56e0
Henry80074 <89064748+Henry80074@users.noreply.github.com>
2021-10-13 17:42:52 +0100

    Update settings.py
    Fix bug

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oisiu#k2oj3)dq%licjdyc(vap=@q$qxz&ikz@e+lil#8k188w'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
